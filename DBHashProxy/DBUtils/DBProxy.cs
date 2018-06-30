3761948c29623162abcf62d519801a10f4eefb9c
bob <dashaomai@qq.com>
2018-06-30 14:47:30 +0800

    初步实现了一个粗暴的 100 拆分数据库离散系统

---
+++ DBHashProxy/DBUtils/DBProxy.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Mono.Data.Sqlite;
+
+namespace DBHashProxy.DBUtils
+{
+    /// <summary>
+    /// Db hash proxy.
+    /// 通过 Hash 的方式对输入查询键进行散列查询的工具类，把超大量的数据，分散到若干个离散的 SQLite 实例当中。
+    /// </summary>
+    public class DBProxy : IDisposable
+    {
+        private string m_path;
+        private int m_dbCount;
+        private SqliteConnection[] m_connections;
+
+        public DBProxy(string path, int dbCount)
+        {
+            m_path = path;
+            m_dbCount = dbCount;
+
+            // 初始化所有的 SQLite 连接
+            m_connections = new SqliteConnection[m_dbCount];
+            for (int i = 0; i < m_dbCount; i++) {
+                var file = m_path + "/" + i + ".db";
+                var conn = new SqliteConnection("Data Source=" + file);
+                conn.Open();
+                using (var command = conn.CreateCommand())
+                {
+                    // 创建表结构
+                    command.CommandText = "CREATE TABLE IF NOT EXISTS rate (kv1 INTEGER NOT NULL, kv2 INTEGER NOT NULL, val INTEGER NOT NULL, CONSTRAINT pk_key_pairs PRIMARY KEY(kv1 ASC, kv2 ASC))";
+                    command.ExecuteNonQuery();
+                }
+
+                m_connections[i] = conn;
+            }
+        }
+
+        public bool Update(string kv1, string kv2, int value) {
+            var hc1 = kv1.GetHashCode();
+            var hc2 = kv2.GetHashCode();
+
+            var index = Math.Abs(hc1 % m_dbCount);
+            var conn = m_connections[index];
+            using (var transaction = conn.BeginTransaction())
+            {
+                int result;
+                using (var command = conn.CreateCommand())
+                {
+                    // 检查记录是否存在
+                    command.CommandText = String.Format("SELECT COUNT(*) FROM rate WHERE kv1={0} AND kv2={1} LIMIT 1", hc1, hc2);
+                    var count = command.ExecuteScalar();
+                    bool isExists = (null != count) ? int.Parse(count.ToString()) > 0 : false;
+
+                    if (isExists) {
+                        command.CommandText = String.Format("UPDATE rate SET val=val+{2} WHERE kv1={0} AND kv2={1}", hc1, hc2, value);
+                    } else {
+                        command.CommandText = String.Format("INSERT OR IGNORE INTO rate (kv1, kv2, val) VALUES({0}, {1}, {2})", hc1, hc2, value);
+                    }
+                    result = command.ExecuteNonQuery();
+                }
+
+                transaction.Commit();
+
+                return 1 == result;
+            }
+        }
+
+        public int Query(string kv1, string kv2) {
+            var hc1 = kv1.GetHashCode();
+            var hc2 = kv2.GetHashCode();
+
+            var index = Math.Abs(hc1 % m_dbCount);
+            var conn = m_connections[index];
+            using (var command = conn.CreateCommand())
+            {
+                command.CommandText = String.Format(
+                    "SELECT val FROM rate WHERE kv1={0} AND kv2={1}",
+                    hc1, hc2
+                );
+
+                var result = command.ExecuteScalar();
+
+                return (null != result ? int.Parse(result.ToString()) : 0);
+            }
+        }
+
+        public void Dispose() {
+            for (int i = 0; i < m_dbCount; i++) {
+                m_connections[i].Dispose();
+                m_connections[i] = null;
+            }
+        }
+    }
+}
---
+++ DBHashProxy/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using DBHashProxy.DBUtils;
+
+namespace DBHashProxy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = System.Environment.CurrentDirectory + "/test_db";
+            int dbCount = 100;
+
+            System.Console.WriteLine(String.Format("准备用：{0}, {1} 初始化数据库", path, dbCount));
+
+            System.IO.Directory.CreateDirectory(path);
+            using (var proxy = new DBProxy(path, dbCount))
+            {
+                Random random = new Random(DateTime.Now.GetHashCode());
+
+                var t1 = DateTime.Now.Ticks;
+
+                // 随机插入数据
+                for (long i = 0, m = 2000000000L; i < m; i++)
+                {
+                    string kv1 = RandomStr(random);
+                    string kv2 = RandomStr(random);
+                    int value = random.Next(1000);
+
+                    proxy.Update(kv1, kv2, value);
+                }
+
+                var t2 = DateTime.Now.Ticks;
+
+                // 进行 1 万次查询
+                for (int i = 0; i < 10000; i++) {
+                    string kv1 = RandomStr(random);
+                    string kv2 = RandomStr(random);
+                    int value = proxy.Query(kv1, kv2);
+                }
+
+                var t3 = DateTime.Now.Ticks;
+
+                var ts1 = new TimeSpan(t2 - t1);
+                var ts2 = new TimeSpan(t3 - t2);
+                var ts3 = new TimeSpan(t3 - t1);
+
+                System.Console.WriteLine(String.Format("消耗时间：{0}, {1}, {2} （毫秒）", ts1.TotalMilliseconds, ts2.TotalMilliseconds, ts3.TotalMilliseconds));
+            }
+        }
+
+        private static char[] m_chars = new char[] {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+        };
+
+        /// <summary>
+        /// 生成 16 位长度的随机字符串
+        /// </summary>
+        /// <returns>The string.</returns>
+        /// <param name="random">Random.</param>
+        private static string RandomStr(Random random)
+        {
+            var count = m_chars.Length;
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < 16; i++) {
+                var index = random.Next(count);
+                builder.Append(m_chars[index]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
